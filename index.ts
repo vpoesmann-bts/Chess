e0885e60ecd44a3aa50d32a5fd34fedfb857f18e
vpoesmann-bts <vpoesmann-bts@gmail.com>
2021-01-28 10:34:06 +0100

    Create grid and html grid

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
-let htmlGrid = document.getElementById("grid");
+const GRID_SIDE: number = 8;
+const BLACK_CELL_CLASS: string = "blackCell";
+const WHITE_CELL_CLASS: string = "whiteCell";
+const CELL_CLASS: string = "cell";
+const ROW_CLASS: string = "row";
+
+let htmlGrid: HTMLDivElement = <HTMLDivElement>document.getElementById("grid");
+let grid: number[][] = [];
+
+function generateGrid(grid: number[][]): void {
+  for (let i: number = 0 ; i < GRID_SIDE ; i++) {
+    grid.push([]);
+    for (let j: number = 0 ; j < GRID_SIDE ; j++) {
+      grid[i].push(0);
+    }
+  }
+}
+
+function generateHTMLGrid(htmlGrid:HTMLDivElement): void {
+  for (let i: number = 0 ; i < GRID_SIDE ; i++) {
+    let row: HTMLDivElement = document.createElement("div");
+    row.classList.add(ROW_CLASS);
+    for (let j: number = 0 ; j < GRID_SIDE ; j++) {
+      let cell: HTMLDivElement = document.createElement("div");
+      if ((i+j) % 2 == 0) {
+        cell.classList.add(BLACK_CELL_CLASS);
+      } else {
+        cell.classList.add(WHITE_CELL_CLASS);
+      }
+      cell.classList.add(CELL_CLASS);
+      row.appendChild(cell);
+    }
+    htmlGrid.appendChild(row);
+  }
+
+}
+
+generateGrid(grid);
+generateHTMLGrid(htmlGrid);
